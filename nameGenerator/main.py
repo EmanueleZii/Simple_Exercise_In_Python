836e8593fc8905e6b231be9b382b99964777977c
Emanuele Zignale <zignale92@gmail.com>
2025-08-25 18:03:09 +0200

    name generator

---
+++ nameGenerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator ")
+city = input("What's the name of the city you grew up in?")
+print(city)
+name = input("What's your name ?")
+print(name)
+print("The name "+ name + city )
+
