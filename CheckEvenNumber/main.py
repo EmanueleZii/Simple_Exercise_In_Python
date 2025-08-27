f9ad8956905f604ce4cd5dec82703fb5e5559918
Emanuele Zignale <zignale92@gmail.com>
2025-08-27 14:49:02 +0200

    Check even number

---
+++ CheckEvenNumber/main.py
@@ -0,0 +1,22 @@
+numberInput = print("What is the number you want to check")
+
+if numberInput / 2 == 0:
+    print("Is Even Number")
+else:
+    print("Is not Even Sorry")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
