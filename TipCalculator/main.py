f4a4e7b50f67397a819ee2a28d907a4049e09121
Emanuele Zignale <zignale92@gmail.com>
2025-08-27 14:12:44 +0200

    tip calculator improve

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -2,28 +2,13 @@ cost = float(input("Inserisci il costo totale: "))
 NumPeople = int(input("Inserisci il numero di persone: "))
 give = float(input("Inserisci la mancia (in percentuale, es. 0.1 per 10%): "))
 
-result = (cost / NumPeople) * (1 + give)
+tip_as_percent = give / 100
+total_tip_amount = cost * tip_as_percent
+total_bill = cost + total_tip_amount
+bill_per_percent = total_bill / NumPeople
+final_amount = round(bill_per_percent, 2)
 
-print(f"Ogni persona deve pagare: {result:.2f}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"Ogni persona deve pagare: {final_amount}")
 
 
 
