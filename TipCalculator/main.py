883e139c754c4caa7ee12a01badee032d738f0ca
Emanuele Zignale <zignale92@gmail.com>
2025-08-27 14:04:09 +0200

    tip calculator

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -1,4 +1,20 @@
-print("ciao " + input("Quale il tuo nome?"))
+cost = float(input("Inserisci il costo totale: "))
+NumPeople = int(input("Inserisci il numero di persone: "))
+give = float(input("Inserisci la mancia (in percentuale, es. 0.1 per 10%): "))
+
+result = (cost / NumPeople) * (1 + give)
+
+print(f"Ogni persona deve pagare: {result:.2f}")
+
+
+
+
+
+
+
+
+
+
 
 
 
