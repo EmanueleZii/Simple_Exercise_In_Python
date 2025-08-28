396d7f6c17723f832a469b8cc8236fd4204d705e
Emanuele Zignale <zignale92@gmail.com>
2025-08-28 14:21:06 +0200

    PriceOrderPizza

--- PriceOrderPizza/main.py
+++ PriceOrderPizza/main.py
@@ -1,5 +1,4 @@
 print("Benvenuto nel programma di ordinazione della pizza!")
-
 size = input("Quale dimensione di pizza desideri? (piccola/media/grande) ")
 pepperoni = input("Vuoi aggiungere pepperoni? (si/no) ")
 extra_cheese = input("Vuoi aggiungere formaggio extra? (si/no) ")
@@ -8,16 +7,18 @@ bill = 0
 
 if size == "piccola":
     bill += 5
+    print("Il costo della pizza piccola è $5")
 elif size == "media":
     bill += 7
+    print("Il costo della pizza media è $7")
 elif size == "grande":
     bill += 9
-
+    print("Il costo della pizza grande è $9")
 if pepperoni == "si":
-    bill += 2
-
+    bill += 3
+    print("Il costo del pepperoni è $3")
 if extra_cheese == "si":
     bill += 1
+    print("Il costo del formaggio extra è $1")
 
 print("Il tuo totale è: $" + str(bill))
-
