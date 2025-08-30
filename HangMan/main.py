257ecbfc1e50e00858bc77b5a172702ba6474e1f
Emanuele Zignale <zignale92@gmail.com>
2025-08-30 19:04:30 +0200

    hangman

--- HangMan/main.py
+++ HangMan/main.py
@@ -1,27 +1,37 @@
 import random
 
+print("Welcome to Hangman!")
+
 word_list = ["python", "java", "javascript", "csharp", "ruby"]
 
 def choose_word():
     word = random.choice(word_list)
-    print("Random word chosen:", word)
+    # print("Random word chosen:", word)  # Puoi togliere questo per non mostrare la parola
     return word
 
-print("Welcome to Hangman!")
-
 chosen_word = choose_word()
-guessed_letter = input("Chosen letter for guessing: ").lower()
+guessed_letters = []
 
-for letter in chosen_word:
-    if letter == guessed_letter:
+while True:
+    display = ""
+    for letter in chosen_word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    print("Current word:", display)
+
+    if display == chosen_word:
+        print("Congratulations! You've guessed the word:", chosen_word)
+        break
+
+    guessed_letter = input("Chosen letter for guessing: ").lower()
+    if guessed_letter in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    if guessed_letter in chosen_word:
         print("Correct guess!")
     else:
         print("Wrong guess!")
-
-
-
-
-
-
-
-
+    guessed_letters.append(guessed_letter)
