5f4033aef2b0b4ea4de5fdd74061191398f62170
Emanuele Zignale <zignale92@gmail.com>
2025-09-01 12:44:35 +0200

    BidAuction improve

--- BlindAuction/main.py
+++ BlindAuction/main.py
@@ -1,17 +1,24 @@
 dict = {}
 
-print("Welcome To bid of chairity")
+print("Welcome To Bid of chairity")
 
-nameofBidders = input("What's your name ? \n ")
-bid = int(input("How Much is you Bid ? \n "))
+while True:
+    nameofBidders = input("What's your name ? \n ")
+    bid = int(input("How Much is you Bid ? \n "))
 
-dict[nameofBidders] = bid
+    dict[nameofBidders] = bid
 
-print(f"Name of bidders: {nameofBidders}")
-print(f"bid: {dict[nameofBidders]}")
+    print(f"Name of bidders: {nameofBidders}")
+    print(f"bid: {dict[nameofBidders]}")
+    continua = str(input("Are there any other bidders? Type 'yes' or 'no' \n")).lower()
+    if continua == 'yes':
+       continue
+    elif continua == 'no':
+        break
 
-
-
+# Alla fine mostra chi ha offerto di più
+winner = max(dict, key=dict.get)
+print(f"\nThe winner is {winner} with a bid of {dict[winner]}!")
 
 
 
